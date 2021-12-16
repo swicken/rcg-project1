a579badad02887e8af39893531b1ea767a527bc1
Scott Wicken <swicken@gmail.com>
2021-12-15 22:23:22 -0500

    module 5 completed

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,19 +1,30 @@
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
+import { useState } from "react";
 
 const AddUser = props => {
+    const [enteredUsername, setEnteredUsername] = useState('');
+    const [enteredAge, setEnteredAge] = useState('');
 
     const addUserHandler = (event) => {
         event.preventDefault();
+        console.log(enteredUsername, enteredAge);
     }
+    const usernameChangeHandler = (event) => {
+        setEnteredUsername(event.target.value);
+    }
+    const ageChangeHandler = (event) => {
+        setEnteredAge(event.target.value);
+    }
+
     return (
         <Card className={styles.input}>
         <form onSubmit={addUserHandler}>
             <label htmlFor="username">Username</label>
-            <input id="username" type="text" />
+            <input id="username" type="text" onChange={usernameChangeHandler}/>
             <label htmlFor="age">Age</label>
-            <input id="age" type="text" />
+            <input id="age" type="text" onChange={ageChangeHandler} />
             <Button type="submit">Add User</Button>
         </form>
         </Card>
