f8d2d2f629ac7cb4e6cf590f63a4b33c8b444eb6
Scott Wicken <swicken@gmail.com>
2021-12-15 22:19:07 -0500

    section 4 done

--- src/components/Users/AddUser.jsx
+++ src/components/Users/AddUser.jsx
@@ -1,5 +1,6 @@
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
+import Button from "../UI/Button";
 
 const AddUser = props => {
 
@@ -13,7 +14,7 @@ const AddUser = props => {
             <input id="username" type="text" />
             <label htmlFor="age">Age</label>
             <input id="age" type="text" />
-            <button type="submit">Add User</button>
+            <Button type="submit">Add User</Button>
         </form>
         </Card>
     )
